390f84107336f30858a517fed2480ea53cc26b29
Kristifor Milchev <km@airzen.io>
2023-10-08 19:59:28 +0300

    Implemting tag locking

--- lib/views/lock_tag/lock_tag_viewmodel.dart
+++ lib/views/lock_tag/lock_tag_viewmodel.dart
@@ -1,11 +1,30 @@
 import 'package:domain/models/tag.dart';
+import 'package:infrastructure/interfaces/infc_service.dart';
 import 'package:presentation/page_view_model.dart';
 
 class LockTagViewModel extends PageViewModel {
+  late INfcService _nfcService;
   Tag tag;
   LockTagViewModel(super.context, this.tag);
 
-  onCancelPressed() {}
+  bool _writeFinished = false;
+  bool get writeFinished => _writeFinished;
 
-  onConfirmPressed() {}
+  ready() {
+    _nfcService = getIt.get<INfcService>();
+    _nfcService.finishSession();
+  }
+
+  onCancelPressed() {
+    observer.getObserver("nfc_scan_again", null);
+  }
+
+  onConfirmPressed() async {
+    await _nfcService.protect();
+    _writeFinished = true;
+    notifyListeners();
+    Future.delayed(Duration(seconds: 2), () {
+      observer.getObserver("nfc_scan_again", null);
+    });
+  }
 }
